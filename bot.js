c5965f71d94b27cf04b69f03b636de0aca022980
ciara <osullc41@tcd.ie>
2019-03-14 12:46:15 +0000

    no errors!

--- bot.js
+++ bot.js
@@ -3,6 +3,48 @@
 
 const { ActivityTypes } = require('botbuilder');
 const { LuisRecognizer } = require('botbuilder-ai');
+const { DialogSet, WaterfallDialog, NumberPrompt, DateTimePrompt, ChoicePrompt, DialogTurnStatus }
+    = require('botbuilder-dialogs');
+    const LOCATION_PROMPT = 'locationPrompt';
+
+    const DIALOG_STATE_ACCESSOR = 'dialogStateAccessor';
+const RESERVATION_ACCESSOR = 'reservationAccessor';
+
+var builder = function () {
+    return applyStyle.apply(builder, arguments);
+};
+
+class dialogue{
+    constructor(conversationState)
+    {
+        // Creates our state accessor properties.
+    // See https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors.
+    this.dialogStateAccessor = conversationState.createProperty(DIALOG_STATE_ACCESSOR);
+    this.reservationAccessor = conversationState.createProperty(RESERVATION_ACCESSOR);
+    this.conversationState = conversationState;
+
+            // Create the dialog set and add the prompts, including custom validation.
+    this.dialogSet = new DialogSet(this.dialogStateAccessor);
+    this.dialogSet.add(new ChoicePrompt(LOCATION_PROMPT));
+
+    // Define the steps of the waterfall dialog and add it to the set.
+    this.dialogSet.add(new WaterfallDialog(RESERVATION_DIALOG, [
+        this.promptForLocation.bind(this),
+    ]));
+    }
+
+    async promptForLocation(stepContext) {
+        // Record the party size information in the current dialog state.
+        stepContext.values.size = stepContext.result;
+    
+        // Prompt for location.
+        return await stepContext.prompt(LOCATION_PROMPT, {
+            prompt: 'Please choose a location.',
+            retryPrompt: 'Sorry, please choose a location from the list.',
+            choices: ['Redmond', 'Bellevue', 'Seattle'],
+        });
+    }
+}
 
 class MyBot {
     constructor(application, luisPredictionOptions, includeApiResults) {
@@ -30,12 +72,12 @@ class MyBot {
                     await turnContext.sendActivity(`LUIS Entity Found: Entity: ${ entityData[index].entity }, 
                     Score: ${ entityData[index].score }, Entity Type ${ entityData[index].type }.`);
                 }
-                    await turnContext.sendActivity(`Ok thank you, your origin is : ${ entityData[0].entity } and your destination you want to go to is : 
-                    ${ entityData[1].entity }  is this correct?`); // checks to see that the destination and origin are correct
+                    await turnContext.sendActivity(`origin is : ${ entityData[0].entity } destination is : 
+                    ${ entityData[1].entity }`); // checks to see that the destination and origin are correct
                         
             } else {
                 // If the top scoring intent was "None" tell the user no valid intents were found and provide help.
-                await turnContext.sendActivity(`Sorry I can only provide information on transportation and traffic`);
+                await turnContext.sendActivity(`Sorry, I can only provide information on transportation and traffic`);
             }
              } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate &&
             turnContext.activity.recipient.id !== turnContext.activity.membersAdded[0].id) {
@@ -44,14 +86,12 @@ class MyBot {
                                             \n I can help you plan your travel route.
                                             \n If you could tell me where you are and where
                                             \n you would like to go. E.g "Bring me from 
-                                            \nTrinity to RDS" 
+                                            \n Trinity to RDS" 
                                             \n Where would you like to go today?`);
         } else if (turnContext.activity.type !== ActivityTypes.ConversationUpdate) {
             // Respond to all other Activity types.
             await turnContext.sendActivity(`[${ turnContext.activity.type }]-type activity detected.`);
         }
-        
-    
     }
 }
 
