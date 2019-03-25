f3b7327ccc15515aa4cee041618039c92149a519
Swapnil Raj <raj.swapnil99@gmail.com>
2019-03-25 21:28:48 +0000

    Add dependency for google maps and string-strip-html
    Implement function to fetch routes from maps

---
+++ maps.js
@@ -0,0 +1,48 @@
+const { Translator } = require('./translator');
+
+const mapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyBkXRGxKdzKB3Lo2MsyzipUxQpccCQP8cA',
+    Promise: Promise
+});
+
+const removeTags = require('string-strip-html');
+
+function log(d) { console.log(d); return d; }
+
+const parseDirections = (directions) => {
+    return directions.json.routes[0].legs[0].steps.map(d => {
+        if (d.steps) {
+            return `${ d.html_instructions }\n${ (d.steps
+                .map(f => f.html_instructions)).join('\n') }`;
+        } else return d.html_instructions;
+    }).join(' ');
+};
+
+const translateErr = async (language) => {
+    const errMsg = 'Sorry I did not understand the locations you specified.';
+    const translatedResponse = await Translator.translate(errMsg, language);
+    return translatedResponse[0].translations[0].text;
+};
+
+const getDirections = async (origin, destination, mode, language) => {
+    const directionsOptions = {
+        origin,
+        destination,
+        transit_mode: [ mode ],
+        language,
+        mode: 'transit',
+        //region: 'ie'
+    };
+
+    return mapsClient
+        .directions(directionsOptions)
+        .asPromise()
+        .then(parseDirections)
+        .then(removeTags)
+        .catch( async () => {return await translateErr(language); });
+};
+module.export = { getDirections };
+(async function main() {
+    console.log(await getDirections('Trinity College Dublin', 'RDS', 'bus', 'es'));
+})();
+
